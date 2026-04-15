c2303cad26b71ca90f6a8c00c21f4d8d2ab3cbbc
rybinakate <imac@Kate.local>
2026-04-15 18:53:32 +0500

    добавлен проект по Каттарской впадине. рассмотрены исходные данные и сценарий 1

--- js/include.js
+++ js/include.js
@@ -5,10 +5,9 @@ document.addEventListener('DOMContentLoaded', () => {
                 <ul>
                     <li><a href="/index.html">Обо мне</a></li>
                     <li><a href="/projects/project1.html">Web-приложение для геоаналитики</a></li>
+                     <li><a href="/projects/qattara-depression.html">Каттарская впадина</a></li>
                    <li><a href="/publications.html">Публикации</a></li>
-                    <!-- <li><a href="/projects/project2.html">Каттарская низменность</a></li -->
-                   <!-- <li><a href="/projects/project3.html">Проект 3</a></li> -->
-                   <!-- <li><a href="/projects/project4.html">Проект 4</a></li> -->
+                    
                     <li><a href="/contacts.html">Контакты</a></li>
                 </ul>
             </nav>
